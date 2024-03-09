7459c7415260edd4fb2f19ac200dc3a157345df1
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-03-08 22:39:43 -0300

    .

--- src/main/java/com/Microservice/clientService/MainApplication.java
+++ src/main/java/com/Microservice/clientService/MainApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
@@ -26,25 +27,9 @@ public class MainApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx =SpringApplication.run(MainApplication.class, args);
-
-		ClientService provinceService = ctx.getBean(ClientService.class);
-
-		ClientRequestDTO clientRequestDTO = new ClientRequestDTO()
-				.name("MARIA")
-				.lastname("BOCCA")
-				.dni(17241664)
-				.cuil(2343463)
-				.birth(LocalDate.now())
-				.telephone("2342454044")
-				.email("batubasilio@hotmail.com")
-				.address(new AddressDTO().id(3)
-						.name("barrera")
-						.number(1200)
-						.location(new LocationDTO()
-								.id(1)));
-
-
+		SpringApplication application = new SpringApplication(MainApplication.class);
+		application.setApplicationStartup(new BufferingApplicationStartup(2048));
+		application.run(args);
 		//System.out.println(provinceService.findAll());
 	}
 
